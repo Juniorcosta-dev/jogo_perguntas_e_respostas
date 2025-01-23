1fc0897c40aabce1a533cbc2b904c082ce748ba7
Junior Costa <juniorcosta100@hotmail.com>
2025-01-23 20:39:17 -0300

    Atualizando o arquivo com mais perguntas e respostas

--- respostas_perguntas.py
+++ respostas_perguntas.py
@@ -1,11 +1,20 @@
 print("Jogo de perguntas e respostas")
 
-pergunta = "Qual o maior animal terrestre? "
-resposta = "Elefante africano"
+perguntaRespostas = {
+    "Qual o maior animal terrestre?": "elefante africano",
+    "Qual é o país mais populoso do mundo?": "china",
+    "Quantos planetas existem no sistema solar?": "Oito"
+}
 
-respostaDoJogador = str(input(pergunta))
+pontuacao = 0
 
-if respostaDoJogador == resposta:
-    print("Você acertou")
-else:
-    print("Você errou")
+for pergunta, respostaCerta in perguntaRespostas.items():
+    respostaDoJogador = input(pergunta + " ").lower()
+
+    if respostaDoJogador == respostaCerta.lower():
+        print("Você acertou!\n")
+        pontuacao += 1
+    else:
+        print(f"Você errou! A resposta correta é: {respostaCerta}\n")
+
+print(f"Fim do jogo! Sua pontuação {pontuacao} de {len(perguntaRespostas)} perguntas.")
